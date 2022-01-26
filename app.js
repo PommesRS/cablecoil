9e8406a4528ce168dd84d35163b015842636d7f2
PommesRS <rspommes@gmail.com>
2022-01-26 21:39:20 +0100

    *

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const cock = document.querySelector(".anim1");
-const video = cock.querySelector("video");
+const anim1 = document.querySelector(".anim1");
+const video = anim1.querySelector("video");
+
+const anim2 = document.querySelector(".anim2");
+const video2 = anim2.querySelector("video");
 //END SECTION
-const end = document.querySelector(".placeholder");
+const end = document.querySelector(".anim1");
+
 
 //SCROLLMAGIC
 const controller = new ScrollMagic.Controller();
 
 //Scenes
 let scene = new ScrollMagic.Scene({
-  duration: 1000,
+  duration: 900,
   triggerElement: video,
   triggerHook: 0
 })
+  .addTo(controller)
 
+let scene2 = new ScrollMagic.Scene({
+  duration: 900,
+  triggerElement: video2,
+  triggerHook: 0
+})
+  .setPin(video2)
   .addTo(controller);
 
 //Video Animation
-let accelamount = 0.5;
+let accelamount = 0.2;
 let scrollpos = 0;
 let delay = 0;
 
